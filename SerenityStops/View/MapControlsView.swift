71807b33992569f25e486d25cdb50a671a7d9fd0
Ayush Kumar Singh <singh.ayush.as@icloud.com>
2024-12-25 13:25:41 +0100

    Fix issues with location permissions Update the UI

---
+++ SerenityStops/View/MapControlsView.swift
@@ -0,0 +1,37 @@
+//
+//  MapControlsView.swift
+//  SerenityStops
+//
+//  Created by Ayush Kumar Singh
+//
+
+import SwiftUI
+import MapKit
+
+/// A view that contains the map controls based on location authorization status
+struct MapControlsView: View {
+    // MARK: - Properties
+    let authStatus: CLAuthorizationStatus
+    let onLocationTap: () -> Void
+    
+    var body: some View {
+        Group {
+            if authStatus == .authorizedWhenInUse || authStatus == .authorizedAlways {
+                MapUserLocationButton()
+            } else {
+                Button(action: onLocationTap) {
+                    Image(systemName: "location.fill")
+                        .imageScale(.large)
+                }
+                .buttonStyle(.plain)
+                .padding(8)
+                .background(.thinMaterial)
+                .cornerRadius(8)
+            }
+            MapCompass()
+        }
+    }
+}
+
+// End of file
+
